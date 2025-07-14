577251669c60bb2b4a71713b400eeb7089b4d3a1
cledson-leite <csbetsonline@gmail.com>
2025-07-14 17:38:23 -0300

    initial setup mobile

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <Text style={{ color: 'white' }}>Home</Text>
+      <Text style={{ color: 'white' }}>Tela inicial do Mobile</Text>
+    </View>
+  );
+}
